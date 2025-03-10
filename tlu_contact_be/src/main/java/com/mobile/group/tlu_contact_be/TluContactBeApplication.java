49af9cf4ec071691392346dd255a4b4b477c5967
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-03-10 21:25:23 +0700

    config firebase

---
+++ tlu_contact_be/src/main/java/com/mobile/group/tlu_contact_be/TluContactBeApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.group.tlu_contact_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TluContactBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TluContactBeApplication.class, args);
+    }
+
+}
