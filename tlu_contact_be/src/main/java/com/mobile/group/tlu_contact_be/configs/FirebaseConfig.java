6807483ba21bad311aa5e3c26f1d6e4f87f5212b
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-04-02 23:49:51 +0700

    add profile api

--- tlu_contact_be/src/main/java/com/mobile/group/tlu_contact_be/configs/FirebaseConfig.java
+++ tlu_contact_be/src/main/java/com/mobile/group/tlu_contact_be/configs/FirebaseConfig.java
@@ -1,8 +1,11 @@
 package com.mobile.group.tlu_contact_be.configs;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
@@ -20,5 +23,15 @@ public class FirebaseConfig {
                 .build();
 
         return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public Firestore firestore(FirebaseApp firebaseApp) {
+        return FirestoreClient.getFirestore(firebaseApp);
+    }
+
+    @Bean
+    public FirebaseAuth firebaseAuth(FirebaseApp firebaseApp) {
+        return FirebaseAuth.getInstance(firebaseApp);
     }
 }
