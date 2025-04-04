21e9a1f5453c0c7c3d634a81502a75dbaa0ab9c4
VuDangHuong <danghuong30082003@gmail.com>
2025-04-04 13:50:21 +0700

    save token and load login

---
+++ TLUContact/app/src/main/java/com/mobile/group/tlucontact/viewmodel/LoginViewModel.kt
@@ -0,0 +1,28 @@
+package com.mobile.group.tlucontact.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+//class LoginViewModel : ViewModel() {
+//    private val _email = MutableLiveData<String>("")
+//    val email: LiveData<String> get() = _email
+//
+//    private val _password = MutableLiveData<String>("")
+//    val password: LiveData<String> get() = _password
+//
+//    private val _rememberMe = MutableLiveData<Boolean>(false)
+//    val rememberMe: LiveData<Boolean> get() = _rememberMe
+//
+//    fun setEmail(email: String) {
+//        _email.value = email
+//    }
+//
+//    fun setPassword(password: String) {
+//        _password.value = password
+//    }
+//
+//    fun setRememberMe(remember: Boolean) {
+//        _rememberMe.value = remember
+//    }
+//}
